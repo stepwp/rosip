f40f157024918377fd12d8c50f94b08e9ec3b341
stepwp <62073828+stepwp@users.noreply.github.com>
2023-05-06 11:33:52 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
+#!/bin/bash
 curl -s https://raw.githubusercontent.com/Loyalsoldier/geoip/release/text/cn.txt |grep -v : |sed -e 's/^/add address=/g' -e 's/$/ list=CNIP/g'|sed -e $'1i\\\n/ip firewall address-list' -e $'1i\\\nremove [/ip firewall address-list find list=CNIP]' -e $'1i\\\nadd address=10.0.0.0/8 list=CNIP comment=private-network' -e $'1i\\\nadd address=172.16.0.0/12 list=CNIP comment=private-network' -e $'1i\\\nadd address=192.168.0.0/16 list=CNIP comment=private-network'>cnip.rsc
